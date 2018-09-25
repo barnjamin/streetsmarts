5b7e0bed398082b916947031b5673b9bda6e8ddc
Ben Guidarelli <ben.guidarelli@gmail.com>
2018-09-25 13:35:06 -0400

    moar

--- launch/runit.sh
+++ launch/runit.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo bash -c "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; roslaunch streetsmarts.launch"
+
+sudo bash -c "
+source /opt/ros/kinetic/setup.bash; 
+source /home/nvidia/catkin_ws/devel/setup.bash; 
+roslaunch ros_mpu6050_node mpu6050.launch 
+"
+
