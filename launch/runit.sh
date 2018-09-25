5078fb64296a6d321739ba58edb5caa72896669e
Ben Guidarelli <ben.guidarelli@gmail.com>
2018-09-25 19:35:35 -0400

    maor

--- launch/runit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo bash -c "
-source /opt/ros/kinetic/setup.bash; 
-source /home/nvidia/catkin_ws/devel/setup.bash; 
-roslaunch ros_mpu6050_node mpu6050.launch 
-"
-
---
+++ launch/start_ss.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo bash -c "source /opt/ros/kinetic/setup.bash; source /home/nvidia/catkin_ws/devel/setup.bash; roslaunch ss.launch"
