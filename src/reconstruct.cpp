546eb18f9c2ba6bc8213be2f155c57d2a4e9bc3d
Ben Guidarelli <ben.guidarelli@gmail.com>
2018-11-10 12:57:44 -0500

    asdfasdf

--- src/reconstruct.cpp
+++ src/reconstruct.cpp
@@ -6,12 +6,13 @@
 
 #include <Cuda/Odometry/RGBDOdometryCuda.h>
 
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 using namespace open3d;
 
 int main(int argc, char * argv[])
 {
-
-
     std::string base_path = "/home/nvidia/streetsmarts/python/latest/";
 
     PinholeCameraIntrinsic intrinsic;
@@ -37,23 +38,22 @@ int main(int argc, char * argv[])
     //auto src_rgbd = RGBDImageCuda(source_i, source_d);
     //auto tgt_rgbd = RGBDImageCuda(target_i, target_d);
 
+    auto src_idx = "74";
+    auto tgt_idx = "78";
 
-    cv::Mat source_color = cv::imread(base_path + "color/0.jpg");
-    cv::cvtColor(source_color, source_color, cv::COLOR_BGR2GRAY);
+    Mat source_color = imread(base_path + "color/"+src_idx+".jpg");
+    cvtColor(source_color, source_color, COLOR_RGB2GRAY);
     source_color.convertTo(source_color, CV_32FC1, 1.0f / 255.0f);
 
-    cv::Mat target_color = cv::imread(base_path + "color/1.jpg");
-    cv::cvtColor(target_color, target_color, cv::COLOR_BGR2GRAY);
+    Mat source_depth = imread(base_path + "depth/"+src_idx+".png", IMREAD_UNCHANGED);
+    source_depth.convertTo(source_depth, CV_32FC1, 0.0001);
+
+    Mat target_color = imread(base_path + "color/"+tgt_idx+".jpg");
+    cvtColor(target_color, target_color, COLOR_RGB2GRAY);
     target_color.convertTo(target_color, CV_32FC1, 1.0f / 255.0f);
 
-    cv::Mat source_depth = cv::imread(base_path + "depth/0.png",
-                                      cv::IMREAD_UNCHANGED);
-    source_depth.convertTo(source_depth, CV_32FC1, 0.001f);
-
-    cv::Mat target_depth = cv::imread(base_path + "depth/1.png",
-                                      cv::IMREAD_UNCHANGED);
-    target_depth.convertTo(target_depth, CV_32FC1, 0.001f);
-
+    Mat target_depth = imread(base_path + "depth/"+tgt_idx+".png", IMREAD_UNCHANGED);
+    target_depth.convertTo(target_depth, CV_32FC1, 0.0001);
 
     source_I.Upload(source_color);
     source_D.Upload(source_depth);
@@ -65,7 +65,6 @@ int main(int argc, char * argv[])
     odometry.Apply(source_D, source_I, target_D, target_I);
 
     std::cout<< "Transform: \n" << odometry.transform_source_to_target_ << std::endl;
-
 
     return EXIT_SUCCESS;
 }
